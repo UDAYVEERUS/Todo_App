63a2309e4bc75ce1877b81d55f24c10075e727e0
Shophree <udayveerus348567@gmail.com>
2023-05-31 09:07:30 +0530

    write code for generate random number

---
+++ src/components/GenerateRandomNumber.js
@@ -0,0 +1,12 @@
+// Generate a random number between min (inclusive) and max (inclusive)
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  // Usage example
+  const minNumber = 1;
+  const maxNumber = 100;
+  const randomNumber = getRandomNumber(minNumber, maxNumber);
+  
+  console.log(randomNumber);
+  
