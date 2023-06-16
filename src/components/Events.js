df58a8d897ea9689fb0314aeaa4d095c8b82866b
Shophree <udayveerus348567@gmail.com>
2023-06-16 09:30:21 +0530

    events double click

--- src/components/Events.js
+++ src/components/Events.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 const Events = () => {
     const [text, setText] = useState("Click me")
     const [bg, setBg] = useState("yellow")
+    const [doubleclick, setDoubleclick]  =  useState("double click")
 
     const changeText = () => {
         // console.log("i am clicked")
@@ -10,10 +11,15 @@ const Events = () => {
         setBg(newbg)
         setText("clicked")
     }
+    const doubleClick = () =>{
+       setDoubleclick("double clicked")
+        setBg("purple")
+    }
   return (
     <div style={{backgroundColor:bg}}>
         <h1>Event in react</h1>
         <button  onClick={changeText}>{text}</button>
+        <button onDoubleClick={doubleClick}>{doubleclick}</button>
     </div>
   )
 }
