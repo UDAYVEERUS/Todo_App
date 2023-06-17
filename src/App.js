4a5d95eef37e8fd04d5918ddad67f9ce39d42986
Shophree <udayveerus348567@gmail.com>
2023-06-17 07:27:05 +0530

    Create 404 error page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as  Router,Switch,Route  } from 'react-router-dom';
 import TodoApp from './components/Todo_app';
 import NumberGenerator from './components/Generate_number';
 import Arrayofobject from './components/ArrayOfObject';
@@ -12,25 +14,27 @@ import ArrayObject from './components/ArrayObject';
 import Events from './components/Events';
 import DIgitalClock from './components/DIgitalClock';
 import IncreDecre from './components/IncreDecre';
+import Error from './components/Error';
 function App() {
   return (
     <>
       <Router>
-        <Routes>
+        <Switch>
 
-        <Route path='/todo' element={<TodoApp />} />
-        <Route path='/' element={<Todo />} />
-        <Route path='/number' element={<NumberGenerator />} />
-        <Route path='/arrayofobject' element={<Arrayofobject />} />
-        <Route path='/showingtext' element={<ShowingText />} />
-        <Route path='/timer' element={<Timer />} />
-        <Route path='/data' element={<Fetchapi />} />
-        <Route path='/mydata' element={<ArrayObject />} />
-        <Route path='/event' element={<Events />} />
-        <Route path='/time' element={<DIgitalClock />} />
-        <Route path='/count' element={<IncreDecre />} />
+          <Route path='/todo' exact component={TodoApp} />
+          <Route path='/' exact component={Todo} />
+          {/* <Route exact path='/number' element={<NumberGenerator />} />
+        <Route exact path='/arrayofobject' element={<Arrayofobject />} />
+        <Route exact path='/showingtext' element={<ShowingText />} />
+        <Route exact path='/timer' element={<Timer />} />
+        <Route exact path='/data' element={<Fetchapi />} />
+        <Route exact path='/mydata' element={<ArrayObject />} />
+        <Route exact path='/event' element={<Events />} />
+        <Route exact path='/time' element={<DIgitalClock />} />
+        <Route exact path='/count' element={<IncreDecre />} /> */}
+          <Route component={Error} />
 
-        </Routes>
+        </Switch>
       </Router>
     </>
   );
---
+++ src/components/Error.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div style={{background:"red"}}>
+            <div style={{textAlign: "center", justifyContent: "center" }}>
+                <h1 style={{ fontSize: "40px" }}>404 Error Page</h1>
+                <p style={{ fontSize: "30px" }}>Sorry this page does not exist</p>
+            </div>
+        </div>
+    )
+}
+
+export default Error
