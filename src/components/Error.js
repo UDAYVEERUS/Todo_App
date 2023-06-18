4efcc7f6a39fe4087e477134f1f5e0f4e0162554
Shophree <udayveerus348567@gmail.com>
2023-06-18 15:27:25 +0530

    Add home page link in error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Error = () => {
     return (
-        <div style={{background:"red"}}>
+        <div >
             <div style={{textAlign: "center", justifyContent: "center" }}>
                 <h1 style={{ fontSize: "40px" }}>404 Error Page</h1>
-                <p style={{ fontSize: "30px" }}>Sorry this page does not exist</p>
+                <p style={{ fontSize: "30px" }}>Sorry this page does not exist.</p>
+                <a href="/" style={{border:"2px blue solid" , background:"green" , padding:"15px", fontSize : "25px", color:"white"}}>Go Back</a>
             </div>
         </div>
     )
