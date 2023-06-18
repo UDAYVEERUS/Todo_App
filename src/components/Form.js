b9454866cc444beab3cbf95db16638ee9b97af3a
Shophree <udayveerus348567@gmail.com>
2023-06-18 14:56:31 +0530

    form complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react'
 
 const Form = () => {
 
-    const [name, setName] = useState()
+    const [name, setName] = useState("")
+    const [fullname, setFullname] = useState("")
 
     const handleChange = (event) => {
         // console.log(event.target.value)
         setName(event.target.value)
     }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setFullname(name)
+    }
     return (
         <div>
-            <h1>Hello : {name}</h1>
-            <input type="text" onChange={handleChange} placeholder='Enter Your Name' />
-            <button>submit</button>
+            <h1>Hello : {fullname}</h1>
+            <input type="text" onChange={handleChange} placeholder='Enter Your Name' value={name} />
+            <button onClick={handleSubmit}>submit</button>
         </div>
     )
 }
