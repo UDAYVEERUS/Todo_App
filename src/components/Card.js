a71850dac9c1c0cbaed37ea981c2c9e4fb8e032a
Shophree <udayveerus348567@gmail.com>
2023-06-18 14:27:59 +0530

    using props make cards function

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Props from './Props'
+
+const Card = (props) => {
+  return (
+    <div>
+        <h1>{props.heading}</h1>
+        <img src={props.imgsrc} alt="_blank" />
+        <p>{props.title}</p>
+        <p>{props.lorem}</p>
+        <button>{props.text}</button>
+        <hr />
+    </div>
+  )
+}
+
+export default Card
